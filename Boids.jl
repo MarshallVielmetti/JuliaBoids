df6090403a12d0cf9f5e2cbb142213aeeb60407b
Marshall Vielmetti <mvielmetti@outlook.com>
2021-07-28 13:35:34 -0500

    Removed extra, added benchmarks

--- Boids.jl
+++ Boids.jl
@@ -1,139 +1,128 @@
-using Base:Float32, aligned_sizeof
 using GLMakie
-
+using BenchmarkTools
 GLMakie.activate!()
 
 ##
 
-# Define constants and types
-
-#Boid Settings
-NUM_BOIDS = 200
-VELOCITY = 150
-VIEW_DISTANCE = 100
+NUM_BOIDS = 100
+VIEW_DISTANCE = 70 
 VIEW_ANGLE = 4π/3
+SIDE_MARGIN = 50
+MARGIN_TURN = 1
 
 # Boid Modifiers
 
-ALIGNMENT_MODIFIER = .3
-COHESION_MODIFIER = .5
-SEPERATION_MODIFIER = .2
-OBSTACLE_MODIFIER = .5
+ALIGNMENT_MODIFIER = 0.1
+COHESION_MODIFIER = 0.1
+SEPERATION_MODIFIER = 0.02
+MIN_DISTANCE = VIEW_DISTANCE / 10
+
+MAX_VELOCITY = 120
+MIN_VELOCITY = 10
+MAX_ACCELERATION = 10
 
 # Animation settings
 FRAMERATE = 30
-SECONDS = 120
+SECONDS = 30
 
 # Plot Settings
-X_WIDTH = 2000
+X_WIDTH = 1000
 Y_WIDTH = 1000
 
 mutable struct Boids
-    xs::Vector{Float64} # m
-    ys::Vector{Float64} # m
-    θ::Vector{Float64} # radians, 0 = right (unit circle)
+    x::Vector{Float64} # m
+    y::Vector{Float64} # m
+    dx::Vector{Float64} # m/s
+    dy::Vector{Float64} # m/s
 end
 
-Boids() = Boids(rand(NUM_BOIDS) * X_WIDTH, rand(NUM_BOIDS) * Y_WIDTH, rand(NUM_BOIDS) * 2π)
+Boids(x) = Boids(rand(x) * X_WIDTH, rand(x) * Y_WIDTH, rand(x)*(MAX_VELOCITY) .- MIN_VELOCITY, rand(x)*(MAX_VELOCITY) .- MIN_VELOCITY)
 
 ##
+
 
 function iterateBoids!(boidsArr)
-    # Moves based on previous heading
-    boidsArr.xs = boidsArr.xs .+ (VELOCITY / FRAMERATE) * cos.(boidsArr.θ) 
-    boidsArr.ys = boidsArr.ys .+ (VELOCITY / FRAMERATE) * sin.(boidsArr.θ) 
-
-    boidsArr.xs = map(loopX!, boidsArr.xs)
-    boidsArr.ys = map(loopY!, boidsArr.ys)
-
-
+    # Iterate positions
+    
     for i in 1:NUM_BOIDS
+        dist = .√(((boidsArr.x .- boidsArr.x[i]) .^ 2) .+ ((boidsArr.y .- boidsArr.y[i]) .^ 2))
 
-        num_in_range = 0
-        θttl = 0
-        xttl = 0
-        yttl = 0
-        θtottl = 0
-
-        dist = .√((boidsArr.xs .- boidsArr.xs[i]) .^ 2 .+ (boidsArr.ys .- boidsArr.ys[i]) .^ 2)
-        θto = atan.((boidsArr.xs .- boidsArr.xs[i]) ./ (boidsArr.ys .- boidsArr.ys[i])) .- boidsArr.θ[i] # Temporarily ignore case of being the same pt or 'inline`
+        xs = []
+        ys = []
+        dxttl = 0
+        dyttl = 0
+        moveX = 0
+        moveY = 0
+        numInRange = 0
 
         for inner in 1:NUM_BOIDS
-            if dist[inner] != 0 && dist[inner] < VIEW_DISTANCE && abs(θto[inner]) < VIEW_ANGLE
-                num_in_range += 1
-                θttl += boidsArr.θ[inner]
-                xttl += boidsArr.xs[inner]
-                yttl += boidsArr.ys[inner]
-                θtottl += θto[inner]
+            if dist[inner] > 0.1 && dist[inner] < VIEW_DISTANCE
+                #Cohesion
+                push!(ys, boidsArr.y[inner])
+                push!(xs, boidsArr.x[inner])
+
+                dxttl += boidsArr.dx[inner]
+                dyttl += boidsArr.dy[inner]
+                #Seperation
+                moveX += boidsArr.x[i] - boidsArr.x[inner]
+                moveY += boidsArr.y[i] - boidsArr.y[inner]
+
+                numInRange += 1
+            end
+
+            if numInRange > 0
+                # COHESION
+                avgX = sum(xs) / numInRange
+                avgY = sum(ys) / numInRange
+                
+                boidsArr.dx[i] += (avgX - boidsArr.x[i]) * COHESION_MODIFIER
+                boidsArr.dy[i] += (avgY - boidsArr.y[i]) * COHESION_MODIFIER
+
+                #SEPERATION
+                boidsArr.dx[i] += moveX * SEPERATION_MODIFIER
+                boidsArr.dy[i] += moveY * SEPERATION_MODIFIER
+
+                #ALIGNMENT
+                avgDX = dxttl / numInRange
+                avgDY = dyttl / numInRange
+
+                boidsArr.dx[i] += (avgDX - boidsArr.dx[i]) * ALIGNMENT_MODIFIER
+                boidsArr.dy[i] += (avgDY - boidsArr.dy[i]) * ALIGNMENT_MODIFIER
+            end
+            
+            #Limit Velocity
+            mag = getMagnitude(boidsArr.dx[i], boidsArr.dy[i])
+            if mag > MAX_VELOCITY
+                boidsArr.dx[i] = (boidsArr.dx[i] / mag) * MAX_VELOCITY
+                boidsArr.dy[i] = (boidsArr.dy[i] / mag) * MAX_VELOCITY
+            end
+
+            #Stay in bounds
+            if boidsArr.x[i] < SIDE_MARGIN
+                boidsArr.dx[i] += MARGIN_TURN
+            elseif boidsArr.x[i] > X_WIDTH - SIDE_MARGIN
+                boidsArr.dx[i] -= MARGIN_TURN
+            end
+            if boidsArr.y[i] < SIDE_MARGIN
+                boidsArr.dy[i] += MARGIN_TURN
+            elseif boidsArr.y[i] > Y_WIDTH - SIDE_MARGIN
+                boidsArr.dy[i] -= MARGIN_TURN
             end
         end
-
-        if X_WIDTH - boidsArr.xs[i] < VIEW_DISTANCE
-            xWallNudge = (π - boidsArr.θ[i]) * OBSTACLE_MODIFIER * X_WIDTH - boidsArr.xs[i]
-        elseif boidsArr.xs[i] < VIEW_DISTANCE
-            xWallNudge = (0 - boidsArr.θ[i]) * OBSTACLE_MODIFIER * boidsArr.xs[i]
-        else
-            xWallNudge = 0
-        end
-
-        if Y_WIDTH - boidsArr.ys[i] < VIEW_DISTANCE
-            yWallNudge = (3π/2 - boidsArr.θ[i]) * OBSTACLE_MODIFIER * Y_WIDTH - boidsArr.ys[i]
-        elseif boidsArr.ys[i] < VIEW_DISTANCE
-            yWallNudge = (π/2 - boidsArr.θ[i]) * OBSTACLE_MODIFIER * boidsArr.ys[i]
-        else
-            yWallNudge = 0
-        end
-
-        if (num_in_range > 0)
-            θavg = θttl / num_in_range
-            xavg = xttl / num_in_range
-            yavg = yttl / num_in_range
-            θtoavg = θtottl / num_in_range
-
-            #ALIGNMENT
-            alignmentNudge = (θavg - boidsArr.θ[i])*ALIGNMENT_MODIFIER
-
-            #COHESION
-            angleToCohesion = atan(xavg - boidsArr.xs[i] / yavg - boidsArr.ys[i])
-            cohesionNudge = (angleToCohesion - boidsArr.θ[i])*COHESION_MODIFIER
-            
-            #SEPERATION
-            angleToSeperation = θtoavg - π
-            if angleToSeperation > 2π
-                angleToSeperation -= 2π
-            elseif angleToSeperation < 0
-                angleToSeperation += 2π
-            end
-            seperationNudge = angleToSeperation - boidsArr.θ[i]*SEPERATION_MODIFIER
-            
-            totalNudge = cohesionNudge + alignmentNudge + seperationNudge + xWallNudge + yWallNudge
-        else
-            totalNudge = rand()*0.1 - .05
-        end
-
-        boidsArr.θ[i] += totalNudge / FRAMERATE
     end
 
-    return boidsArr
+
+    boidsArr.x = boidsArr.x .+ (boidsArr.dx ./ FRAMERATE)
+    boidsArr.y = boidsArr.y .+ (boidsArr.dy ./ FRAMERATE)
 end
 
-function loopX!(xval)
-    if xval > X_WIDTH
-        xval = 0
-    elseif xval < 0
-        xval = X_WIDTH
-    end
-    return xval
+
+function getMagnitude(dx, dy)
+    return √(dx^2 + dy^2)
 end
-function loopY!(yval)
-    if yval > Y_WIDTH
-        yval = 0
-    elseif yval < 0
-        yval = Y_WIDTH
-    end
-    return yval
-end
+
 ##
+
 # Define Figure
 
 fig = Figure(resolution=(X_WIDTH, Y_WIDTH))
@@ -146,17 +135,18 @@ hidedecorations!(ax)
 limits!(ax, 0, X_WIDTH, 0, Y_WIDTH)
 
 ##
-boids = Boids()
+boids = Boids(NUM_BOIDS)
 
 timestamps = range(0, SECONDS, step=1 / FRAMERATE)
 
 record(fig, "randimation.mp4", timestamps; framerate = FRAMERATE) do i
     empty!(ax)
-
-
     iterateBoids!(boids)
     # tans = atan.(boids.yv ./ boids.xv)
-    scatter!(ax, boids.xs, boids.ys, color = :black)
+    scatter!(ax, boids.x, boids.y, color = :black)
 end
 
-##" 
+##
+
+@benchmark iterateBoids!(b) setup=(b = Boids(NUM_BOIDS)) seconds=10
+
